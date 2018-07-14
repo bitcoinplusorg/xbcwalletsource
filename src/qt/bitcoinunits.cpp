53e64c8a60745a62f1978feca92b10a8fdd3df1b
Peter Bushnell <bushsolo@gmail.com>
2018-07-14 08:55:52 +0100

    BTC > XBC

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -40,9 +40,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("BTC");
-    case mBTC: return QString("mBTC");
-    case uBTC: return QString::fromUtf8("μBTC");
+    case BTC: return QString("XBC");
+    case mBTC: return QString("mXBC");
+    case uBTC: return QString::fromUtf8("μXBC");
     default: return QString("???");
     }
 }
@@ -51,9 +51,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("Bitcoins");
-    case mBTC: return QString("Milli-Bitcoins (1 / 1" THIN_SP_UTF8 "000)");
-    case uBTC: return QString("Micro-Bitcoins (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
+    case BTC: return QString("BitcoinPlus");
+    case mBTC: return QString("Milli-BitcoinPlus (1 / 1" THIN_SP_UTF8 "000)");
+    case uBTC: return QString("Micro-BitcoinPlus (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
     default: return QString("???");
     }
 }
