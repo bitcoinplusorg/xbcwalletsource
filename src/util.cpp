30638c583dc16a9c2acbaa2133d0c8a68f535aee
Peter Bushnell <bushsolo@gmail.com>
2018-07-14 08:57:47 +0100

    conf, pid and datadir

--- src/util.cpp
+++ src/util.cpp
@@ -99,8 +99,8 @@ namespace boost {
 
 using namespace std;
 
-const char * const BITCOIN_CONF_FILENAME = "bitcoin.conf";
-const char * const BITCOIN_PID_FILENAME = "bitcoind.pid";
+const char * const BITCOIN_CONF_FILENAME = "bitcoinplus.conf";
+const char * const BITCOIN_PID_FILENAME = "bitcoinplusd.pid";
 
 map<string, string> mapArgs;
 map<string, vector<string> > mapMultiArgs;
@@ -461,7 +461,7 @@ boost::filesystem::path GetDefaultDataDir()
     // Unix: ~/.bitcoin
 #ifdef WIN32
     // Windows
-    return GetSpecialFolderPath(CSIDL_APPDATA) / "Bitcoin";
+    return GetSpecialFolderPath(CSIDL_APPDATA) / "bitcoinplus";
 #else
     fs::path pathRet;
     char* pszHome = getenv("HOME");
@@ -471,10 +471,10 @@ boost::filesystem::path GetDefaultDataDir()
         pathRet = fs::path(pszHome);
 #ifdef MAC_OSX
     // Mac
-    return pathRet / "Library/Application Support/Bitcoin";
+    return pathRet / "Library/Application Support/bitcoinplus";
 #else
     // Unix
-    return pathRet / ".bitcoin";
+    return pathRet / ".bitcoinplus";
 #endif
 #endif
 }
