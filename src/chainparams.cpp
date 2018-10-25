0aa12893f77ba64d9d6cdb8e052b8ecb1bfd71eb
Peter Bushnell <bushsolo@gmail.com>
2018-10-25 08:15:45 +0100

    Unique names for seeders

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -112,8 +112,8 @@ public:
         assert(genesis.hashMerkleRoot == uint256S("0x81d205aec871e7998b1313f195603159a2e6388ced896536dce76bf1410fc039"));
 
         // Note that of those with the service bits flag, most only support a subset of possible options
-        vSeeds.push_back(CDNSSeedData("xbcbitcoinplus.co.uk", "seed1.xbcbitcoinplus.co.uk", false));
-        vSeeds.push_back(CDNSSeedData("xbcbitcoinplus.co.uk", "seed2.xbcbitcoinplus.co.uk", false));
+        vSeeds.push_back(CDNSSeedData("ns1.xbcbitcoinplus.co.uk", "seed1.xbcbitcoinplus.co.uk", false));
+        vSeeds.push_back(CDNSSeedData("ns2.xbcbitcoinplus.co.uk", "seed2.xbcbitcoinplus.co.uk", false));
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,25);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,85);
