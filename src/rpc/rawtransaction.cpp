ff3be4a029e9f2a2b9ae0440c8a6ad39d4b51d89
Bitcoinplusbus <contactus@bitcoinplus.org>
2021-08-02 10:44:13 +0100

    Include timedata header

--- src/rpc/rawtransaction.cpp
+++ src/rpc/rawtransaction.cpp
@@ -20,6 +20,7 @@
 #include "script/script_error.h"
 #include "script/sign.h"
 #include "script/standard.h"
+#include "timedata.h"
 #include "txmempool.h"
 #include "uint256.h"
 #include "utilstrencodings.h"
